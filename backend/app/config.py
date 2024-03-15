b8ded59c4debdf0c8abc0bcbd0e01fba802fb23f
Zain Abdeen <zainabiddeen19@gmail.com>
2024-03-15 00:00:00 -0400

    Commit: config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -5,3 +5,4 @@ SECRET_KEY = dotenv_values()["SECRET_KEY"]
 CLOUDINARY_NAME = dotenv_values()["CLOUDINARY_CLOUD_NAME"]
 CLOUDINARY_API_KEY = dotenv_values()["CLOUDINARY_API_KEY"]
 CLOUDINARY_API_SECRET = dotenv_values()["CLOUDINARY_API_SECRET"]
+# Random change 11087
